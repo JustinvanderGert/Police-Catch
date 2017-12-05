2d3ba7cead8b76e450d13fb1c9d88dff90fad905
JustinGert <justinvandergert@outlook.com>
2017-12-05 12:59:36 +0100

    unity update

---
+++ Unity Files/Police Catch/Assets/Scripts/MissionObjective.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MissionObjective : MonoBehaviour
+{
+    public GameObject stationCameraSpot;
+    public GameObject station;
+    public GameObject panel;
+    public bool isOn;
+
+    public Button missions;
+
+	void Start ()
+    {
+        stationCameraSpot.SetActive(false);
+        isOn = false;
+	}
+	
+	void Update ()
+    {
+        float distance = Vector3.Distance(station.transform.position, transform.position);
+        if(distance <= 2 && Input.GetButtonDown("E"))
+        {
+            stationCameraSpot.SetActive(true);
+        }
+	}
+
+    public void TaskOnClick()
+    {
+        isOn = !isOn;
+        panel.SetActive(isOn);
+    }
+}
