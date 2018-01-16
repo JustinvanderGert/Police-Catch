7e847dac0d40ca46f39ff49e72285b4a47c1a64e
JustinGert <justinvandergert@outlook.com>
2018-01-16 13:54:38 +0100

    Update

--- Unity Files/Police Catch/Assets/Scripts/Gun.cs
+++ Unity Files/Police Catch/Assets/Scripts/Gun.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    public GameObject taser;
+    public GameObject pistol;
     public GameObject bullet;
 
+    GameObject activeWeapon;
 
-	void Update ()
+
+    void Update()
     {
-		if(Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1")) { activeWeapon = taser; }
+        if (Input.GetButtonDown("Fire2")) { activeWeapon = pistol; }
+
+		if(Input.GetButtonDown("Fire1") && activeWeapon == taser)
         {
             Instantiate(bullet, gameObject.transform.position, gameObject.transform.rotation);
         }
-	}
+
+        if(pistol == activeWeapon) { Instantiate(pistol, gameObject.transform.position, gameObject.transform.rotation); }
+        else { DestroyImmediate(pistol); }
+
+        if (taser == activeWeapon) { Instantiate(taser, gameObject.transform.position, gameObject.transform.rotation); }
+        else { DestroyImmediate(taser); }
+    }
 }
