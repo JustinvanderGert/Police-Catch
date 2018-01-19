0dbd281c6115b85e0042f5725b042b3a2b3aaa72
JustinGert <justinvandergert@outlook.com>
2018-01-19 11:50:14 +0100

    unity update

--- Unity Files/Police Catch/Assets/Scripts/Gun.cs
+++ Unity Files/Police Catch/Assets/Scripts/Gun.cs
@@ -4,27 +4,43 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    public GameObject taser;
-    public GameObject pistol;
     public GameObject bullet;
-
+    public GameObject pistol;
+    public GameObject taser;
     GameObject activeWeapon;
+    bool spawned;
 
+
+    private void Start()
+    {
+        pistol.SetActive(false);
+        pistol.SetActive(false);
+
+        activeWeapon = null;
+    }
 
     void Update()
     {
-        if (Input.GetButtonDown("Fire1")) { activeWeapon = taser; }
-        if (Input.GetButtonDown("Fire2")) { activeWeapon = pistol; }
+        pistol.transform.position = gameObject.transform.position;
+        taser.transform.position = gameObject.transform.position;
 
-		if(Input.GetButtonDown("Fire1") && activeWeapon == taser)
+        if (Input.GetButtonDown("One") && activeWeapon != taser)
+        {
+            activeWeapon = taser;
+            taser.SetActive(true);
+            pistol.SetActive(false);
+        }
+
+        if (Input.GetButtonDown("Two") && activeWeapon != pistol)
+        {
+            activeWeapon = pistol;
+            pistol.SetActive(true);
+            taser.SetActive(false);
+        }
+
+		if(Input.GetButtonDown("Fire1") && activeWeapon != null)
         {
             Instantiate(bullet, gameObject.transform.position, gameObject.transform.rotation);
         }
-
-        if(pistol == activeWeapon) { Instantiate(pistol, gameObject.transform.position, gameObject.transform.rotation); }
-        else { DestroyImmediate(pistol); }
-
-        if (taser == activeWeapon) { Instantiate(taser, gameObject.transform.position, gameObject.transform.rotation); }
-        else { DestroyImmediate(taser); }
     }
 }
